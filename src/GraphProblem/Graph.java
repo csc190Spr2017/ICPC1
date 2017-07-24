12ac7846764c285ee71e3d8672e154dcb7f9e6e6
Dr. Evil <xfu@gmail.com>
2017-07-24 12:55:15 -0400

    igraph2

--- src/GraphProblem/Graph.java
+++ src/GraphProblem/Graph.java
@@ -37,9 +37,69 @@ public class Graph {
         return G;
     }
     
+    /* properly set the "layer" of all nodes so that we actually get a tree out
+    of the acyclic undirecitonal graph.
+    Layer1 nodes are those leaf nodes with NO neighbors or only one neibor.
+    The requirement is that FOR ANY EDGE: the two nodes CANNOT have the
+    same layer number.
+    For any neighbors, the one with HIGHER layer number is the PARENT.
+    */
+    
+    protected ArrayList<Node> highestLayer;
     public void markLayers(){
+        //1. collect all layer1 nodes (the ones with up TO 1 neighbor)
+        ArrayList<Node> Layer1 = getLayer1();
+        setLayerNumber(Layer1, 1);
         
-        return;
+        ArrayList<Node> curLayer = Layer1;
+        ArrayList<Node> nextLayer = new ArrayList<Node>();
+        int curLayerNumber = 1;
+        //2. for each layer
+        while(curLayer.size()!=0){
+            //2.1 break ties randomly, until for each node in the layer there is no tie
+            
+            for(int i = 0; i < curLayer.size(); i++){
+                Node currentNode = curLayer.get(i);
+                ArrayList<Edge> CNedges = currentNode.edges;
+                 for(int j = 0; j < CNedges.size(); j++){
+                     Edge CNcurrentEdge = CNedges.get(j);
+                     if(CNcurrentEdge.node1.layer == CNcurrentEdge.node2.layer){
+                         currentNode.setLayer(curLayerNumber+1);
+                         nextLayer.add(currentNode);
+                         break;
+                     }
+                 }
+            }
+            curLayerNumber++;
+            highestLayer = curLayer;
+            curLayer = nextLayer;
+            nextLayer = new ArrayList<Node>();
+        }
+    }
+    
+    protected ArrayList<Node> getLayer1(){
+        ArrayList<Node> layer1 = new ArrayList<>();
+        for(int i = 0; i < nodes.size(); i++){
+            if(nodes.get(i).edges.size() <= 1){
+                layer1.add(nodes.get(i));
+            }
+        }
+        return layer1;
+    }
+    
+    protected void setLayerNumber(ArrayList<Node> layer, int num){
+        for(int i = 0; i < layer.size(); i++){
+            layer.get(i).setLayer(num);
+        }
+    }
+    
+    public int getPairWiseDist(){
+        this.markLayers();
+        int sum = 0;
+        for(int i = 0; i < this.highestLayer.size(); i++){
+            sum += this.highestLayer.get(i).sumPairWiseDist();
+        }
+        return sum;
     }
     
 }
--- src/GraphProblem/Node.java
+++ src/GraphProblem/Node.java
@@ -29,13 +29,76 @@ public class Node {
         this.layer = layer;
     }
     
-    public int sumToDistChildren(){
-        return 0;
+    /**
+      get all children: that is all neighbors have LOWER layer number than me
+      */
+    public ArrayList<Node> getChildren(){
+        ArrayList<Node> arr = new ArrayList();
+        for(int i=0; i<this.edges.size(); i++){
+            Edge edge = this.edges.get(i);
+            Node otherNode = edge.node1==this? edge.node2: edge.node1;
+            if(otherNode.layer< this.layer){
+                arr.add(otherNode);
+            }
+        }
+        return arr;
     }
     
+    protected int treeSize = -1; //initial
+    /**
+     * Get the size of the tree with ME as the root, me included
+     * @return 
+     */
+    public int getTreeSize(){
+        if(this.treeSize!=-1) return this.treeSize;
+        ArrayList<Node> arrChildren = this.getChildren();
+        int totalSize = 0;
+        for(int i=0; i<arrChildren.size(); i++){
+            Node child = arrChildren.get(i);
+            int size = child.getTreeSize();
+            totalSize+=size;
+        }
+        return totalSize;
+    }
+    
+    /* return the sum of distances from the node (as the root of a subtree) to
+    all of its descendents (lower layer numbers). If there are 5 nodes, then
+    it's the sum of 4 distances.
+    */
+    protected int sd = -1;
+    public int sumToDistChildren(){
+        if(sd != -1){
+            return sd;
+        }
+        sd = 0;
+        for(int i = 0; i < this.edges.size(); i++){
+            Edge edge = this.edges.get(i);
+            Node nb = edge.node1==this? edge.node2: edge.node1;
+            sd = edge.cost * nb.getTreeSize() + nb.sumToDistChildren();
+        }
+        return sd;
+    }
+    
+    /* return the sum of pairwise distances of ALL THE NODES of the 
+    tree starting from me (me included). If there are 5 nodes, then it's the sum 
+    of 10 distances (pairwise)
+    */
     public int sumPairWiseDist(){
-        return 0;
+        int sum = 0;
+        for(int i = 0; i < this.edges.size(); i++){
+            Edge edge_i = this.edges.get(i);
+            Node n_i = edge_i.node1==this? edge_i.node2: edge_i.node1;
+            sum += n_i.sumPairWiseDist();
+            for(int j = i +1; j < this.edges.size(); j++){
+                Edge edge_j = this.edges.get(j);
+                Node n_j = edge_j.node1==this? edge_j.node2: edge_j.node1;       
+                sum += n_i.sumToDistChildren() * n_j.getTreeSize() + 
+                        n_j.sumToDistChildren() * n_i.getTreeSize() +
+                        (edge_i.cost + edge_j.cost) * (n_j.getTreeSize() + n_i.getTreeSize());
+            }
+        }
+        return sum;
     }
-    }
+    
     
 }
